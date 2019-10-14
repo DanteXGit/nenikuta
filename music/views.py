3b259fbcb00b1c56ee7515cf39d6e4db05d04470
DanteX <alisher.nugmanoff@gmail.com>
2019-10-14 17:55:18 +0600

    added buyng1

--- music/views.py
+++ music/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import track
 from .forms import Buy
-import requests
 # Create your views here.
 def index(request):
     key = "48e7qUxn9T7RyYE1MVZswX1FRSbE6iyCj2gCRwwF3Dnh5XrasNTx3BGPiMsyXQFNKQhvukniQG8RTVhYm3iPq18AaSqHW8Xm6Y4H1Z2gUHthx5B6vN8o4qqVkk118k9tbZeN8pF79cdd4uRyQyRa45SxiN4tkKBNq1NuYaVSGapNcek9QBne8DaPhtUnk"
